ae69176f250818a9f46034287180f862de913a8d
CeciliaDeLosSantos <ceci271020@gmail.com>
2024-08-17 22:53:22 -0300

    agregar eventos.js, style.css y index.html actualizado

---
+++ eventos.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let container = document.getElementById('container');
+    
+   
+    container.addEventListener('click', function() {
+        alert('Hola! Soy el div');
+    });
+});
