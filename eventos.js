030d3887aeed04bf2b77f9e874e17fa00f8f88e8
CeciliaDeLosSantos <ceci271020@gmail.com>
2024-08-18 00:56:01 -0300

    solucion

--- eventos.js
+++ eventos.js
@@ -1,8 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
     let container = document.getElementById('container');
-    
+    let button= document.getElementById ('saludarBoton');
    
     container.addEventListener('click', function() {
         alert('Hola! Soy el div');
+    });
+
+    button.addEventListener('click', function(event) {
+        event.stopPropagation(); 
+        alert('Hola!');
     });
 });
